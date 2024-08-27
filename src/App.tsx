01c49723445f0c188bef01ad48a38f22ee3cda76
SuriyaRamesh <suriya.ram@anywhere.co>
2024-08-27 17:36:12 +0530

    add expandabletext component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Cart from "./components/Cart";
 import UpdateGameState from "./components/UpdateGameState";
 import AddTopping from "./components/AddTopping";
 import ShoppingCartUpdate from "./components/ShoppingCartUpdate";
+import ExpandableText from "./components/ExpandableText";
 
 
 const handleSelectItem = (item: string) => console.log(item);
@@ -28,6 +29,9 @@ function App() {
       <UpdateGameState />
       <AddTopping />
       <ShoppingCartUpdate />
+      <ExpandableText>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio adipisci, reprehenderit minus dolorem iusto rem ratione quam ex unde! Quas dolor maxime magnam dolorem est obcaecati! Deserunt eveniet et temporibus veniam laboriosam harum enim ex. Sint ipsa quibusdam quia autem. Facilis suscipit nisi perferendis. Ipsa rerum aperiam laborum excepturi recusandae ullam dicta dolorem quibusdam, modi, commodi, nesciunt similique. Dolores nesciunt optio totam at eveniet odio deleniti atque aliquid! At cupiditate consequuntur sint distinctio sequi iste voluptas! Eveniet blanditiis ad maiores magnam ratione, ullam placeat eos sequi, iste vel quaerat consequatur sapiente earum? Quia unde ullam accusamus expedita. Molestias, iure quos.
+      </ExpandableText>
     </div>
   )
 }
