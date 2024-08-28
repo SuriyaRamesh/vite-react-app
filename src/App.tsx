53a7d88845be93b720fe4ddb2acd948acb28a71b
SuriyaRamesh <suriya.ram@anywhere.co>
2024-08-28 17:04:20 +0530

    create a form

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import UpdateGameState from "./components/UpdateGameState";
 import AddTopping from "./components/AddTopping";
 import ShoppingCartUpdate from "./components/ShoppingCartUpdate";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 
 const handleSelectItem = (item: string) => console.log(item);
@@ -19,7 +20,7 @@ function App() {
   const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
 
   return (
-    <div className="App p-5">
+    <div className="App">
      <ListGroup heading="Numbers List" items={items} onSelectItem={handleSelectItem} />
      {showAlert && <Alert OnClose={() => setShowAlert(false)}><h3>There is an Error!!!</h3></Alert>}
      <Button OnClickHandle={() => setShowAlert(true)} >My Button</Button>
@@ -32,6 +33,7 @@ function App() {
       <ExpandableText>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio adipisci, reprehenderit minus dolorem iusto rem ratione quam ex unde! Quas dolor maxime magnam dolorem est obcaecati! Deserunt eveniet et temporibus veniam laboriosam harum enim ex. Sint ipsa quibusdam quia autem. Facilis suscipit nisi perferendis. Ipsa rerum aperiam laborum excepturi recusandae ullam dicta dolorem quibusdam, modi, commodi, nesciunt similique. Dolores nesciunt optio totam at eveniet odio deleniti atque aliquid! At cupiditate consequuntur sint distinctio sequi iste voluptas! Eveniet blanditiis ad maiores magnam ratione, ullam placeat eos sequi, iste vel quaerat consequatur sapiente earum? Quia unde ullam accusamus expedita. Molestias, iure quos.
       </ExpandableText>
+      <Form />
     </div>
   )
 }
